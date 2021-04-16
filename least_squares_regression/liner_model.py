557b8d86a2a93af7336eb8c1b8ab83af705c29c5
Kenkentake <keeeentake0908@gmail.com>
2021-04-16 14:35:05 +0900

    Rename handout files

--- least_squares_regression/liner_model.py
+++ least_squares_regression/liner_model.py
@@ -10,17 +10,17 @@ import matplotlib.pyplot as plt
 np.random.seed(0)  # set the random seed for reproducibility
 
 
-def generate_sample(xmin, xmax, sample_size):
-    x = np.linspace(start=xmin, stop=xmax, num=sample_size)
+def generate_sample(XMIN, XMAX, SAMPLE_SIZE):
+    x = np.linspace(start=XMIN, stop=XMAX, num=SAMPLE_SIZE)
     pix = np.pi * x
     target = np.sin(pix) / pix + 0.1 * x
-    noise = 0.05 * np.random.normal(loc=0., scale=1., size=sample_size)
+    noise = 0.05 * np.random.normal(loc=0., scale=1., size=SAMPLE_SIZE)
     return x, target + noise
 
 
 def calc_design_matrix(x):
-    sample_size = len(x)
-    phi = np.empty(shape=(sample_size, 31))  # design matrix
+    SAMPLE_SIZE = len(x)
+    phi = np.empty(shape=(SAMPLE_SIZE, 31))  # design matrix
     phi[:, 0] = 1.
     phi[:, 1::2] = np.sin(x[:, None] * np.arange(1, 16)[None] / 2)
     phi[:, 2::2] = np.cos(x[:, None] * np.arange(1, 16)[None] / 2)
@@ -28,20 +28,20 @@ def calc_design_matrix(x):
 
 
 # create sample
-sample_size = 50
-xmin, xmax = -3, 3
-x, y = generate_sample(xmin=xmin, xmax=xmax, sample_size=sample_size)
+SAMPLE_SIZE = 50
+XMIN, XMAX = -3, 3
+x, y = generate_sample(XMIN=XMIN, XMAX=XMAX, SAMPLE_SIZE=SAMPLE_SIZE)
 
 # calculate design matrix
 phi = calc_design_matrix(x)
 
 # solve the least square problem
-theta = np.linalg.solve(phi.T.dot(phi), phi.T.dot(y[:, None]))
+theta = np.linalg.solve(np.dot(phi.T, phi), np.dot(phi.T, y[:, None]))
 
 # create data to visualize the prediction
-X = np.linspace(start=xmin, stop=xmax, num=5000)
+X = np.linspace(start=XMIN, stop=XMAX, num=5000)
 Phi = calc_design_matrix(X)
-prediction = Phi.dot(theta)
+prediction = np.dot(Phi, theta)
 
 # visualization
 plt.clf()
