5c560eb5ca360752b7a089074752e783af77d8f6
Kenkentake <keeeentake0908@gmail.com>
2021-07-24 22:47:01 +0900

    Add homework

--- transfer_learning/cwls.py
+++ transfer_learning/cwls.py
@@ -18,9 +18,28 @@ def generate_data(n_total, n_positive):
 
 
 def cwls(train_x, train_y, test_x):
-    # implement this function
     pass
 
+def cwls(train_x, train_y, test_x):
+    train_x = np.concatenate([train_x, np.ones((len(train_x), 1))], axis=1)
+    test_x = np.concatenate([test_x, np.ones((len(test_x), 1))], axis=1)
+    xx = np.sqrt(np.sum((train_x[:, None] - train_x[None]) ** 2, axis=2))
+    xX = np.sqrt(np.sum((train_x[:, None] - test_x[None]) ** 2, axis=2))
+    ratio_of_class = np.empty(2)
+    b = np.empty(2)
+    A = np.empty((2, 2))
+    for i in [0, 1]:
+        ratio_of_class[i] = np.mean(np.where(train_y == i, 1., 0.))
+        b[i] = np.mean(xX[train_y == i])
+        for j in [0, 1]:
+            A[i, j] = np.mean(
+                xx[(train_y == i)[:, None] * (train_y == j)[None]])
+    pi = (A[0, 1] - A[1, 1] - b[0] + b[1]) / (2 * A[0, 1] - A[0, 0] - A[1, 1])
+    pi = np.clip(pi, 0., 1.)
+    pi = np.array([pi, 1 - pi])
+    w = pi[train_y] / ratio_of_class[train_y]
+    z = np.where(train_y == 0, 1., -1.)
+    return np.linalg.solve(train_x.T.dot(w[:, None] * train_x),train_x.T.dot(w * z))
 
 def visualize(train_x, train_y, test_x, test_y, theta):
     for x, y, name in [(train_x, train_y, 'train'), (test_x, test_y, 'test')]:
